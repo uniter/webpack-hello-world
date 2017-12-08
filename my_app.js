deabd3b9e0e1cfefb35cc4d0c0ac3a7e88cf35c3
Daniel Phillimore <dan@ovms.co>
2017-12-08 18:22:33 +0000

    Correct an example code comment

--- my_app.js
+++ my_app.js
@@ -1,5 +1,4 @@
-// Run the PHP file, capture its result Closure
-// and cast it to a JavaScript function we can call
+// Fetch the PHP module (but don't actually run it yet)
 const binderModule = require('./binder.php')();
 
 // Hook stdout and stderr up to the console
@@ -10,6 +9,8 @@ binderModule.getStderr().on('data', function (data) {
     console.warn(data);
 });
 
+// Run the PHP module, capture its result Closure
+// and cast it to a JavaScript function we can call
 const binder = binderModule.execute().getNative();
 
 // Call the PHP closure function returned from binder.php, from JavaScript!
