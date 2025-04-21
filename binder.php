03330090767772e89136ecd8acfa2c7d18f7ca5d
Dan Phillimore <dan@ovms.co>
2025-04-21 01:31:14 +0100

    Add "Throw" button to demonstrate line numbers support

--- binder.php
+++ binder.php
@@ -12,4 +12,11 @@ return function ($document) {
     $document->getElementById('worldButton')->addEventListener('click', function () use ($messageBox) {
         $messageBox->textContent = 'You clicked World!';
     });
+
+    $document->getElementById('throwButton')->addEventListener('click', function () use ($messageBox) {
+        $messageBox->textContent = 'Aw snap - check the console for the error! ' .
+            'Try setting a breakpoint in your browser\'s devtools.';
+
+        throw new Exception('This is a test exception');
+    });
 };
---
+++ uniter.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    settings: {
+        phptojs: {
+            // Enable line numbers in PHP errors.
+            lineNumbers: true
+        }
+    }
+};
